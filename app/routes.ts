7281646addea487b8ece07b1e9f0f2cd0ec55734
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2023-01-14 15:07:58 +0530

    -

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const routes = express.Router()
 
 routes.get('/getdata',(req: any, res: any)=>{
-    res.status(200).send('Hello Typescript')
+    res.status(200).send('Welcome to Typescript World')
 })
 
 routes.get('/getstatus',(req: any, res: any)=>{
